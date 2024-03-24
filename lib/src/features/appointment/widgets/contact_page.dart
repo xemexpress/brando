fe8f76a7f5250bacbb1d8146920fdacbfef5485c
Jason Luo <22487340+xemexpress@users.noreply.github.com>
2024-03-24 19:09:53 +0800

    Updated

--- lib/src/features/appointment/widgets/contact_page.dart
+++ lib/src/features/appointment/widgets/contact_page.dart
@@ -1,7 +1,6 @@
 import 'package:brando/src/core/core.dart';
 import 'package:brando/src/features/appointment/controllers/controllers.dart';
 import 'package:brando/src/features/appointment/widgets/widgets.dart';
-import 'package:brando/src/features/auth/view/auth_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
